abb75f477ccf7e4ffaceaadea2e2fac743190dc9
ebeecroft1 <eric.beecroft@gmail.com>
2022-06-05 13:50:56 +1000

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About">
+            <p>Catch me outside, how about that?</p>
+        </Layout>
+    );
+};
+
+export default AboutPage;
